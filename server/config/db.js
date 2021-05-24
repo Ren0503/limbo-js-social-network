7511411eec158fc7f14049c32db4243ac1af63b2
Ren0503 <illuminati0440@gmail.com>
2021-05-24 19:41:27 +0700

    Add utils folder

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URI, {
+        const conn = await mongoose.connect(process.env.MONGO_URL, {
             useUnifiedTopology: true,
             useNewUrlParser: true,
             useCreateIndex: true,
