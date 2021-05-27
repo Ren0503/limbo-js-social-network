1ce6fed7f2e227696e3cece0f8125c70303556c2
Ren0503 <illuminati0440@gmail.com>
2021-05-27 15:25:22 +0700

    add constants for client

---
+++ client/src/constants/MessageType.js
@@ -0,0 +1,11 @@
+/**
+ * Available Message Types
+ */
+export const MessageType = {
+    SUCCESS: 'SUCCESS',
+    INFO: 'INFO',
+    WARNING: 'WARNING',
+    ERROR: 'ERROR',
+};
+
+export const allMessageTypes = Object.keys(MessageType);
---
+++ client/src/constants/NotificationType.js
@@ -0,0 +1,8 @@
+/**
+ * Available Notifications types
+ */
+export const NotificationType = {
+    LIKE: 'LIKE',
+    FOLLOW: 'FOLLOW',
+    COMMENT: 'COMMENT'
+};
