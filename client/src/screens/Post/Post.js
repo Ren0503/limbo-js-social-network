d3b30275560516b34d4f8e0655535717e966d885
Ren0503 <illuminati0440@gmail.com>
2021-06-12 21:20:16 +0700

    Add post screens

---
+++ client/src/screens/Post/Post.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withRouter } from 'react-router-dom';
+
+import { Content } from 'styles/Layout';
+import PostPopup from 'components/Post/PostPopup';
+
+/**
+ * Post detail
+ */
+const Post = ({ match }) => {
+    return (
+        <Content>
+            <PostPopup usedInModal={false} id={match.params.id} />
+        </Content>
+    );
+}
+
+Post.propTypes = {
+    match: PropTypes.object.isRequired,
+};
+
+export default withRouter(Post);
