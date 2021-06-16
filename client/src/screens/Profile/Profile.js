2fee552b570f9520899f770b1a407927d8b22f43
Ren0503 <illuminati0440@gmail.com>
2021-06-16 21:17:54 +0700

    Add profile screen

---
+++ client/src/screens/Profile/Profile.js
@@ -0,0 +1,82 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withRouter } from 'react-router-dom';
+import { useQuery } from '@apollo/client';
+import styled from 'styled-components';
+
+import { Container, Spacing } from 'styles/Layout';
+import ProfileInfo from './ProfileInfo';
+import ProfilePosts from './ProfilePosts';
+import CreatePost from 'components/Post/CreatePost';
+import Skeleton from 'components/Shared/Skeleton';
+import NotFound from 'components/Shared/NotFound';
+import Head from 'components/Shared/Head';
+
+import { GET_USER } from 'graphql/user';
+
+import { useStore } from 'store';
+
+const Root = styled.div`
+    width: 100%;
+
+    @media (min-width: ${(p) => p.theme.screen.lg}) {
+        margin-left: ${(p) => p.theme.spacing.lg};
+        padding: 0;
+    }
+`;
+
+/**
+ * User Profile
+ */
+const Profile = ({ match }) => {
+    const [{ auth }] = useStore();
+    const { username } = match.params;
+    const { data, loading, error } = useQuery(GET_USER, {
+        variables: { username },
+    });
+
+    const renderContent = () => {
+        if (loading) {
+            return (
+                <Container padding="xxs">
+                    <Skeleton height={350} />
+                    <Container maxWidth="sm">
+                        <Spacing top="lg" bottom="lg">
+                            <Skeleton height={82} />
+                        </Spacing>
+                    </Container>
+                </Container>
+            );
+        }
+
+        if (error || !data.getUser)     return <NotFound />;
+
+        return (
+            <Container padding="xxs">
+                <ProfileInfo user={data.getUser} />
+
+                <Container maxWidth="sm">
+                    <Spacing top="lg" bottom="lg">
+                        {username === auth.user.username && <CreatePost />}
+                    </Spacing>
+
+                    <ProfilePosts username={username} />
+                </Container>
+            </Container>
+        );
+    };
+
+    return (
+        <Root>
+            <Head title={username} /> 
+
+            {renderContent()}
+        </Root>
+    );
+}
+
+Profile.propTypes = {
+    match: PropTypes.object.isRequired,
+};
+
+export default withRouter(Profile);
